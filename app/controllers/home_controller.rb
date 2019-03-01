da18dec09c2908396bbb9b7d7d4fb78196b42a8e
jsonjordan <jtjordan13@gmail.com>
2019-02-28 19:13:47 -0500

    Created simple home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+    # require 'github/markup'
+  
+    #displays README in github fashion (homepage)
+    def readme
+    # @html = GitHub::Markup.render('README.md', File.read("README.md"))
+    end
+  
+  end
