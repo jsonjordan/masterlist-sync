10a0b4ab139b9a4f5067307fa3fbccf98e266c56
jsonjordan <jtjordan13@gmail.com>
2019-03-02 15:30:16 -0500

    Set up basic user#show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'home#home'
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  get '/users/user/:id' => 'users#show', :as => 'user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
