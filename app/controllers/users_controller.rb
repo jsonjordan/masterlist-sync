89c8ceae9c7c10f86954d4cbadfd760d9dda9953
jsonjordan <jtjordan13@gmail.com>
2019-03-02 18:41:36 -0500

    Set up RSpotify basics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   
     def show
       @user = User.find params[:id]
+      @playlists = RSpotify::User.find(@user.uid).playlists
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module MasterlistSync
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    RSpotify::authenticate(ENV["SpotifyClientID"], ENV["SpotifyClientSecret"])
+
   end
 end
