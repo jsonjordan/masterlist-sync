911e39289788223c8fbf7da30392cce476dcfdf5
jsonjordan <jtjordan13@gmail.com>
2019-03-16 21:36:28 -0400

    user home now shows real data - mostly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    binding.pry
+    @minion = @user.minion_playlists.where("is_valid = ? and disabled = ?", "true", "false").order(:id)
+    @master = @user.master_playlists.where(disabled: false).order(:id)
   end
 end
