f18837bc68fd67afbb77fc417de90747994d29b6
jsonjordan <jtjordan13@gmail.com>
2019-03-28 08:09:57 -0400

    changed dashboard page name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  def show
+  def dashboard
     @user = User.find(params[:id])
     @minion = @user.minion_playlists.where("is_valid = ? and disabled = ?", "true", "false").order(:id)
     @master = @user.master_playlists.where(disabled: false).order(:id)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  get '/users/user/:id' => 'users#show', :as => 'user'
+  get '/user/:id/dashboard' => 'users#dashboard', :as => 'user'
   get '/master/:playlist_name' => 'master_playlists#show'
   post '/minion/:playlists_id' => 'minion_playlists#make_master'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
