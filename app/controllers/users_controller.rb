876cdb120d55f7b2a67be8eb39d4ed39a178f416
Jason Jordan <jtjordan13@gmail.com>
2022-05-11 18:47:02 -0400

    Updated security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  require 'json'
 
   def dashboard
-    @user = current_user
+    if ENV["ApprovedUsers"].json_parse.include? current_user.id
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+    end
     @minion = @user.minion_playlists.where("is_valid = ? and disabled = ?", "true", "false").order(:id)
     @master = @user.master_playlists.where(disabled: false).order(:id)
   end
